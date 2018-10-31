0f81a0d5f7f710a431a8d0f887c9801096cc1bbb
Gabriel Duquette <gabrielduquette@gmail.com>
2018-10-31 14:58:32 -0400

    Updates

---
+++ precache-manifest.09d1e4981686c627e0aaaf7a57e03725.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c9a989e78651f543249e",
+    "url": "/game-of-life/static/css/main.a3a47bc5.chunk.css"
+  },
+  {
+    "revision": "c9a989e78651f543249e",
+    "url": "/game-of-life/static/js/main.c9a989e7.chunk.js"
+  },
+  {
+    "revision": "5c3432191b019b11b59f",
+    "url": "/game-of-life/static/css/1.c8c130a0.chunk.css"
+  },
+  {
+    "revision": "5c3432191b019b11b59f",
+    "url": "/game-of-life/static/js/1.5c343219.chunk.js"
+  },
+  {
+    "revision": "48c5f91bb349b88c84a7",
+    "url": "/game-of-life/static/js/runtime~main.48c5f91b.js"
+  },
+  {
+    "revision": "b5ddb87f81308741626f99d41bd21af4",
+    "url": "/game-of-life/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/game-of-life/precache-manifest.e1b10c1fcd7859bcfd73690a1314a967.js"
+  "/game-of-life/precache-manifest.09d1e4981686c627e0aaaf7a57e03725.js"
 );
 
 workbox.clientsClaim();
